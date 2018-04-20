d47cc13f8db486f0a0d01af5aee5b1d45e9e027f
hvu <hvusyslogicinclocal@outlook.com>
2018-04-20 13:38:58 -0500

    GetStartedWithhvuIoTHub

---
+++ CreateDeviceId/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceId
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string hvuIotHubConnectionString = @"HostName=hvuConnectTheDots.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=8puc3+vC0/N5gpkPy5GpecoeguL6+DnCzp/6SI5Fj84=";
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(hvuIotHubConnectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "hvuConnectTheDotsFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
---
+++ ReadDeviceToCloudMessages/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReadDeviceToCloudMessages
+{
+    class Program
+    {
+        static string connectionString = @"HostName=hvuConnectTheDots.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=8puc3+vC0/N5gpkPy5GpecoeguL6+DnCzp/6SI5Fj84=";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+        static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            System.Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
